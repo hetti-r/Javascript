89e0309581600449cd3555e43b18fb4f27c7a0d7
Hetti <hetti.ronnemaa@gmail.com>
2024-01-19 12:58:12 +0200

    data exercise

---
+++ Week2/data_types.js
@@ -0,0 +1,28 @@
+/*
+- Create a variable named age and assign it an integer value.
+- Create a variable named name and assign it a string value.
+- Create a variable named isStudent and assign it a boolean value.
+- Create a variable named hobbies and assign it an array containing a few string values (e.g., hobbies).
+- Create an info object with two properties: schoolName (a string) and grade (an integer).
+
+- Use console.log() to print each variable to the console.
+
+- Use the typeof operator to check the data type of each variable and print the result using console.log(). */
+
+let Age = 18;
+let Name = "Maija";
+let isStudent = true;
+let Hobbies = ["skating", "gardening", "hiking"];
+
+const infoObject = {
+  schoolName: "Kiva Koulu",
+  grade: 9,
+};
+
+console.log(
+  typeof Age,
+  typeof Name,
+  typeof isStudent,
+  typeof Hobbies,
+  typeof infoObject
+);
