3550a0bb3f1dd2c3518ecea2e2867d0c4c125c80
Hetti <hetti.ronnemaa@gmail.com>
2024-01-22 12:39:11 +0200

    add

---
+++ Week2/numbers_01.js
@@ -0,0 +1,23 @@
+// Complete the function convertNumberToString such that it converts the number it receives into a string.
+// Complete the function convertStringToNumber such that it converts the string it receives into a number.
+
+function convertNumberToString(number) {}
+
+function convertStringToNumber(string) {}
+
+// Sample usage - do not modify
+console.log(convertNumberToString(42)); // "42"
+console.log(convertNumberToString(97)); // "97"
+console.log(convertNumberToString(11)); // "11"
+
+console.log(convertNumberToString("42")); // 42
+console.log(convertNumberToString("97")); // 97
+console.log(convertNumberToString("11")); // 11
+
+function convertNumberToString(number) {
+  return number.toString();
+}
+
+function convertStringToNumber(string) {
+  return string.parseInt();
+}
---
+++ Week2/numbers_02.js
@@ -0,0 +1,12 @@
+// Define a function to convert a temperature from Fahrenheit to Celsius.
+// This function uses the formula (Fahrenheit - 32) * 5/9
+// The resulting temperature is rounded to one decimal place for precision.
+
+// Sample usage - do not modify
+console.log(fahrenheitToCelsius(21)); // "-6,1"
+
+function fahrenheitToCelsius(Fahrenheit) {
+  return (Fahrenheit - 32) / 1.8;
+}
+
+let Fahrenheit = 6;
