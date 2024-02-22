b978c97bace6a84edf1f764fc842820cdf15fe03
unknown <hetti.ronnemaa@gmail.com>
2024-02-22 11:34:50 +0200

    torstai

---
+++ Week6/objects_01.js
@@ -0,0 +1,14 @@
+// Task 1: Creating and logging an object
+/*
+Create an object named `person` with properties: `name` set to "Alex" and `age` set to 30.
+Log this object to the console.
+*/
+
+// Write your object here
+
+const person = {
+    name: 'Alex',
+    age: 30,
+  };
+
+  console.log(person)
